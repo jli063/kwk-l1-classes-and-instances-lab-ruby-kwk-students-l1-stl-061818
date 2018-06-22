b2fb784b384c304f1130e5409f7da325dd4be3e6
jli063 <jli063@rsdmo.org>
2018-06-22 18:38:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,13 @@
 # person.rb
 class Person
-  @adele_goldberg = adele_goldberg
-  @alan_kay = alan_kay
+  def name(adele_goldberg, alan_kay)
+    @adele_goldberg = adele_goldberg
+    @alan_kay = alan_kay
+  end
 end
+
+adele_goldberg = Person.new 
+adele_goldberg.name ="Adele Goldberg"
+
+alan_kay=Person.new
+alan_kay.name = "Alan Kay"
